d7c299290b202339a268b2c571645b07c309b3c2
Mudasir Ahmad <mudasirahmadmca@gmail.com>
2024-06-26 19:06:13 +0530

    first commit

---
+++ Js/1.js
@@ -0,0 +1,13 @@
+const month="jan"
+switch (month) {
+    case "jan":
+        console.log("Please welcome");
+        break;
+        case "feb":
+        console.log("Muzamil");
+        break;
+
+
+    default:
+        break;
+}
